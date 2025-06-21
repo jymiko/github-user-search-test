8411643abc6b1b9e5c0ac17f378765473c33d59c
jymiko <jymiko92@gmail.com>
2025-06-21 15:26:51 +0700

    Update base URLs to match new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-user-search-test/',
+  base: '/simple-github-users-search/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
